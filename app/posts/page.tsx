9dc4dc40951ed6b04e857e2729d235fb0795d440
Adam HAMDI <adamhamdi12@gmail.com>
2024-02-14 15:14:29 +0100

    second commit

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,14 +6,11 @@ interface Post {
   userId?: number;
   id?: number;
   title?: string;
-  completed?: boolean;
+  body?: string;
 }
 
-export default function Posts() {
-  const [postDetail, setPostDetail] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
+const fetchPosts= async() => {
+   
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts',
         {
@@ -22,26 +19,29 @@ export default function Posts() {
           }
         });
         const postData = await response.json();
-        setPostDetail(postData);
         console.log(postData);
+         return postData
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
  
-    fetchData();
-  }, []);
+   export default async function Posts(){
+
+   const posts : Post[] = await fetchPosts()
 
   return (
     <div>
       <div >
         <h1>Posts List</h1>
         <ul>
-          {Array.isArray(postDetail) && postDetail.map(post => (
+          {posts.map(post => (
             <li className="post-block" key={post.id}>
-              <Link rel="stylesheet" href={`/posts/${post.id}`} >
-              {post.id} - {post.title}
-              </Link> </li>
+              <Link className='font-bold' rel="stylesheet" href={`/posts/${post.id}`} >
+            <u> {post.id} - {post.title}</u> 
+              </Link> 
+              <p className='text-gray-500'>{post.body}</p>
+              </li>
           ))}
         </ul>
       </div>
